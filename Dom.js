b6c08a1327a96dcc5f7a309331a9d7fff15de70d
asadhossain <developerasad007@gmail.com>
2023-05-22 19:21:50 +0600

    ADD

--- Dom.js
+++ Dom.js
@@ -1,9 +1,15 @@
 
+
+var text = document.getElementById("text");
+
 var ourImg = document.getElementById("images");
 function thisIsAsadImage(){
     ourImg.src = "Asad-Hossain (1) (1).jpg"
+    text.innerHTML = "This is Asad's Image";
 }
 var ourImg1 = document.getElementById("images1");
 function thisIsDolonImage(){
     ourImg1.src = "color.jpg"
+    text.innerHTML = "This is Dolon's Image";
+
 }
