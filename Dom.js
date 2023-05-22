1a9e42f6c90017be29dbc68f4c0291c64bd701ed
asadhossain <developerasad007@gmail.com>
2023-05-22 22:42:13 +0600

    A

--- Dom.js
+++ Dom.js
@@ -13,3 +13,10 @@ function thisIsDolonImage(){
     text.innerHTML = "This is Dolon's Image";
 
 }
+function add() {
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    var result= n1+n2;
+    console.log(result);
+    document.getElementById('result').innerHTML=result;
+}
